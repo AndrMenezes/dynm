e1e590c1933f50bd702a1d900f5cd618a1c806f8
Eduardo Gabriel Pinheiro <eduardo.pinheiro@murabei.com>
2023-07-27 12:52:35 -0300

    multiple transfer function suport

--- src/dlm_transfer_function.py
+++ src/dlm_transfer_function.py
@@ -1,8 +1,9 @@
 """Utils functions."""
 import numpy as np
 import copy
-from src.algebra import _calc_predictive_mean_and_var, _calc_aR
+from src.algebra import _calc_predictive_mean_and_var
 from src.algebra import _build_Gnonlinear, _build_W
+from scipy.linalg import block_diag
 
 
 class TransferFunction():
@@ -49,15 +50,20 @@ class TransferFunction():
             self.W = W
             self.estimate_W = False
 
-        self.F = self._build_F()
-        self.G = self._build_G(x=0)
-
         # Get index for blocks
-        block_idx = np.cumsum([order, order])
-        self.index_dict = {
-            'response': np.arange(0, block_idx[0]),
-            'decay': np.arange(block_idx[0], block_idx[1])
-        }
+        self.index_dict = {}
+
+        for n in range(ntfm):
+            block_idx = n * (2 * order + 1) + np.cumsum([order, order, 1])
+            self.index_dict[n] = {
+                'all': np.arange(n * (2 * order + 1), block_idx[2]),
+                'response': np.arange(n * (2 * order + 1), block_idx[0]),
+                'decay': np.arange(block_idx[0], block_idx[1]),
+                'pulse': np.arange(block_idx[1], block_idx[2])}
+
+        # Build F and G
+        self.F = self._build_F()
+        self.G = self._build_G(x=np.repeat(0, ntfm))
 
     def forecast(self, k: int = 1):
         """Forecast y at time (t+k).
@@ -80,43 +86,52 @@ class TransferFunction():
         return np.ravel(f), np.ravel(q)
 
     def _build_F(self):
-        F = np.zeros(2 * self.order + 1)
-        F[0] = 1
+        F = np.array([])
+
+        for i in range(self.ntfm):
+            Fi = np.zeros(2 * self.order + 1)
+            Fi[0] = 1
+
+            F = np.hstack((F, Fi))
+
         return F.reshape(-1, 1)
 
-    def _build_G(self, x: float):
+    def _build_G(self, x: np.array):
         m = self.m
         order = self.order
-        G = _build_Gnonlinear(m=m, order=order)
+        ntfm = self.ntfm
 
-        x = np.ravel(x)
-        H = np.zeros(G.shape[0]).reshape(-1, 1)
-        H[0, 0] = x
-        G = np.block([[G, H], [H.T * 0, 1]])
+        G = np.empty([0, 0])
+        for n in range(ntfm):
+            idx_ = np.concatenate((
+                self.index_dict.get(n).get('response'),
+                self.index_dict.get(n).get('decay'),
+                self.index_dict.get(n).get('pulse')))
+
+            m_ = m[idx_]
+            Gi = _build_Gnonlinear(m=m_.reshape(-1, 1), order=order)
+
+            xn = np.ravel(x[n])
+            H = np.zeros(Gi.shape[0]).reshape(-1, 1)
+            H[0, 0] = xn
+            Gi = np.block([[Gi, H], [H.T * 0, 1]])
+            G = block_diag(G, Gi)
 
         return G
 
     def _build_h(self, G: np.array):
+        ntfm = self.ntfm
         G_ = copy.deepcopy(G)
-        idx = np.ix_(self.index_dict.get('response'),
-                     self.index_dict.get('decay'))
 
-        G_[idx] = G_[idx] * 0.0
+        for n in range(ntfm):
+            idx = np.ix_(self.index_dict.get(n).get('response'),
+                         self.index_dict.get(n).get('decay'))
+            G_[idx] = G_[idx] * 0.0
 
         m = self.m.T
         h = (G_ - G) @ m.T
 
         return h
-
-    def _calc_aR(self):
-        m = self.m
-        order = self.order
-
-        a, R = _calc_aR(mod=self)
-        G = self._build_Gnonlinear(m=m, order=order)
-        a += self._build_h(G=G)
-
-        return a, R
 
     def _build_P(self, G: np.array):
         return G @ self.C @ G.T
@@ -124,7 +139,12 @@ class TransferFunction():
     def _build_W(self, P: np.array):
         if self.estimate_W:
             W = _build_W(mod=self, P=P)
-            W[1:self.order, 1:self.order] = W[1:self.order, 1:self.order] * 0.0
+
+            for n in range(self.ntfm):
+                idx = np.ix_(self.index_dict.get(n).get('all')[1:],
+                             self.index_dict.get(n).get('all')[1:])
+
+                W[idx] = W[idx] * 0.0
         else:
             W = self.W
         return W
