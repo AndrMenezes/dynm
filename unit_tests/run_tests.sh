e977f40006ddc8e70e38e89c95fef193217b7e94
AndrMenezes <andrefelipemaringa@gmail.com>
2023-10-02 13:09:16 +0100

    merging into seas into main

--- unit_tests/run_tests.sh
+++
@@ -1,4 +0,0 @@
-python -m unittest unit_tests/test__ar.py &
-python -m unittest unit_tests/test__tf.py &
-python -m unittest unit_tests/test__ar_tf.py &
-python -m unittest unit_tests/test__multiple_tf.py
